3da9a0a9a31b70c4ba19bb70e450c2fbbd7a0a28
unknown <piterpriadi@gmail.com>
2023-07-16 23:27:56 +0700

    tambah background di login dan register

--- lib/login.dart
+++ lib/login.dart
@@ -13,122 +13,129 @@ class _LoginState extends State<Login> {
   bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width / 4,
-              height: MediaQuery.of(context).size.height / 5,
-            ),
-            Icon(
-              Icons.book,
-              size: 75,
-            ),
-            Text(
-              "KOMIK",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 1.5,
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                controller: TextEditingController(),
-                decoration: InputDecoration(
-                    hintText: 'Enter Email Here',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(11),
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(11),
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 73, 255, 94)),
-                    ),
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Colors.white,
-                    )),
-              ),
-            ),
-            Container(
-              height: 10,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 1.5,
-              child: TextField(
-                obscureText: _isObscure,
-                decoration: InputDecoration(
-                    hintText: 'Enter Password Here',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(11),
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(11),
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 73, 255, 94)),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _isObscure ? Icons.visibility : Icons.visibility_off,
-                        color: Colors.white,
-                      ),
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/bglogin.jpg"), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: Text('Login'),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: MediaQuery.of(context).size.height / 5,
+                ),
+                Icon(Icons.book, size: 75, color: Colors.black),
+                Text(
+                  "KOMIK",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: Colors.black),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: TextEditingController(),
+                    decoration: InputDecoration(
+                        hintText: 'Enter Email Here',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(11),
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(11),
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 73, 255, 94)),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+                Container(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: TextField(
+                    obscureText: _isObscure,
+                    decoration: InputDecoration(
+                        hintText: 'Enter Password Here',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(11),
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(11),
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 73, 255, 94)),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isObscure
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isObscure = !_isObscure;
+                            });
+                          },
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+                Container(
+                  height: 30,
+                ),
+                SizedBox(
+                    height: 40,
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shadowColor: Colors.lightGreen,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          backgroundColor: Color.fromARGB(255, 99, 218, 103),
+                          foregroundColor: Colors.white),
                       onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return Main();
+                        }));
                       },
-                    ),
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.white,
+                      child: Text('Login'),
                     )),
-              ),
+                ElevatedButton(
+                    onPressed: () {
+                      Route route =
+                          MaterialPageRoute(builder: (context) => Register());
+                      Navigator.push(context, route);
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        foregroundColor: Colors.white,
+                        elevation: 0,
+                        side: const BorderSide(
+                          width: 1.0,
+                          color: Colors.transparent,
+                        )),
+                    child: const Text(
+                      'Make an account',
+                    ))
+              ],
             ),
-            Container(
-              height: 30,
-            ),
-            SizedBox(
-                height: 40,
-                width: 200,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shadowColor: Colors.lightGreen,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      backgroundColor: Color.fromARGB(255, 99, 218, 103),
-                      foregroundColor: Colors.white),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return Main();
-                    }));
-                  },
-                  child: Text('Login'),
-                )),
-            ElevatedButton(
-                onPressed: () {
-                  Route route =
-                      MaterialPageRoute(builder: (context) => Register());
-                  Navigator.push(context, route);
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    foregroundColor: Colors.white,
-                    elevation: 0,
-                    side: const BorderSide(
-                      width: 1.0,
-                      color: Colors.transparent,
-                    )),
-                child: const Text(
-                  'Make an account',
-                ))
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
--- lib/register.dart
+++ lib/register.dart
@@ -13,135 +13,144 @@ class _RegisterStates extends State<Register> {
   bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(leading: null, title: Text('Register')),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width / 4,
-              height: MediaQuery.of(context).size.height / 5,
-            ),
-            Icon(
-              Icons.book,
-              size: 75,
-            ),
-            Text(
-              "KOMIK",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 1.5,
-              child: TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  controller: TextEditingController(),
-                  decoration: InputDecoration(
-                      hintText: 'Enter Email Here',
-                      border: OutlineInputBorder(
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/bgregis.jpg"), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(leading: null, title: Text('Register')),
+          body: Center(
+            child: Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: MediaQuery.of(context).size.height / 5,
+                ),
+                Icon(Icons.book, size: 75, color: Colors.black),
+                Text(
+                  "KOMIK",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: Colors.black),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: TextEditingController(),
+                      decoration: InputDecoration(
+                          hintText: 'Enter Email Here',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide: BorderSide(color: Colors.black)),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(11),
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 73, 255, 94)),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.white,
+                          )),
+                      style: TextStyle(color: Colors.black)),
+                ),
+                Container(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: TextField(
+                    obscureText: _isObscure,
+                    decoration: InputDecoration(
+                        hintText: 'Enter Password Here',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(11),
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(11),
-                          borderSide: BorderSide(color: Colors.black)),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(11),
-                        borderSide:
-                            BorderSide(color: Color.fromARGB(255, 73, 255, 94)),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Colors.white,
-                      )),
-                  style: TextStyle(color: Colors.black)),
-            ),
-            Container(
-              height: 10,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 1.5,
-              child: TextField(
-                obscureText: _isObscure,
-                decoration: InputDecoration(
-                    hintText: 'Enter Password Here',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(11),
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(11),
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 73, 255, 94)),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _isObscure ? Icons.visibility : Icons.visibility_off,
-                        color: Colors.white,
-                      ),
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 73, 255, 94)),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isObscure
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isObscure = !_isObscure;
+                            });
+                          },
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+                Container(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: TextField(
+                    obscureText: _isObscure,
+                    decoration: InputDecoration(
+                        hintText: 'Password Confirmation',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(11),
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(11),
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 73, 255, 94)),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isObscure
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isObscure = !_isObscure;
+                            });
+                          },
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+                Container(
+                  height: 30,
+                ),
+                SizedBox(
+                    height: 40,
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shadowColor: Colors.lightGreen,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          backgroundColor: Color.fromARGB(255, 99, 218, 103),
+                          foregroundColor: Colors.white),
                       onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return Main();
+                        }));
                       },
-                    ),
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.white,
+                      child: Text('Register'),
                     )),
-              ),
+              ],
             ),
-            Container(
-              height: 10,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 1.5,
-              child: TextField(
-                obscureText: _isObscure,
-                decoration: InputDecoration(
-                    hintText: 'Password Confirmation',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(11),
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(11),
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 73, 255, 94)),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _isObscure ? Icons.visibility : Icons.visibility_off,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
-                      },
-                    ),
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.white,
-                    )),
-              ),
-            ),
-            Container(
-              height: 30,
-            ),
-            SizedBox(
-                height: 40,
-                width: 200,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shadowColor: Colors.lightGreen,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      backgroundColor: Color.fromARGB(255, 99, 218, 103),
-                      foregroundColor: Colors.white),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return Main();
-                    }));
-                  },
-                  child: Text('Register'),
-                )),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
